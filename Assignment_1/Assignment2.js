ffa8b2178e5aa36715bbcbd62d9c4c765ff0fa0b
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-10-06 16:35:18 +0200

    final

---
+++ Assignment_1/Assignment2.js
@@ -0,0 +1,21 @@
+/*
+   @author: Elias Posluk
+   Student-id: AG3064
+   Uppgift: Assignment 1 part 2
+   Malmö University
+   Course: DA281A Web Development II - Client - 
+   @date 10/09/2021
+   
+*/
+'use strict';
+
+//Uppgift2 
+document.write('<h1>Uppgift 2!</h1>');
+
+const str1 = "Tisdag";
+const str2 = "Hamburgare";
+const str3 = "I'll be back";
+
+console.log("1. ", str1, " = ", str1.substring(0, 3));
+console.log("2. ", str2, " = ", str2.substring(3, 10));
+console.log("3. ", str3, " = ", str3.substring(5, 12));
