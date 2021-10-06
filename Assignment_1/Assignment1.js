e7956be47abc3426746204a30bb0f35f00152b67
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-10-06 16:29:24 +0200

    final

---
+++ Assignment_1/Assignment1.js
@@ -0,0 +1,24 @@
+/*
+   @author: Elias Posluk
+   Student-id: AG3064
+   Uppgift: Assignment 1 part 1
+   Malmö University
+   Course: DA281A Web Development II - Client - 
+   @date 10/09/2021
+   
+*/
+"use strict";
+
+//Uppgift1
+document.write('<h1>Uppgift 1!</h1>', "Utifrån kodexemplet nedan ska ni färdigställa samtliga utskrifter. Alla utskrifter ska ge värdet true och ni får endast använda följande operatorer en gång:");
+
+//Using operator "<"
+console.log("5 * 2  < 12", 5 * 2 < 12);
+//Using operator "!="
+console.log("55!= 22", 55 != 22);
+//Using operator "=="
+console.log("16 / 4  == 4", 16 / 4 == 4);
+//Using operator "<="
+console.log("8 + 2  <= 128", 8 + 2 <= 128);
+//Using operator ">"
+console.log("32 * 8  > 255", 32 * 8 > 255);
