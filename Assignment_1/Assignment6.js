6e08617471fbe5e794de88cd7b2313c28d62d8c7
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-10-06 16:38:56 +0200

    final

---
+++ Assignment_1/Assignment6.js
@@ -0,0 +1,20 @@
+/*
+   @author: Elias Posluk
+   Student-id: AG3064
+   Uppgift: Assignment 1 part 6
+   Malmö University
+   Course: DA281A Web Development II - Client - 
+   @date 10/09/2021
+   
+*/
+
+"use strict";
+
+//Uppgift6 
+
+document.write('<h1>Uppgift 6!</h1>');
+
+var values = [3, 5, "Jane", true, 144, false];
+
+console.log("Värdet i arrayen utskriven utan ändring: ", values);
+console.log("Utskrift i motsats ordning (baklänges): ", values.reverse());
