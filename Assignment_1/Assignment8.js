9badae8cc02bdf58d9ad0c3b3a202bf1924475c8
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-10-06 16:39:41 +0200

    final

---
+++ Assignment_1/Assignment8.js
@@ -0,0 +1,17 @@
+/*
+   @author: Elias Posluk
+   Student-id: AG3064
+   Uppgift: Assignment 1 part 8
+   Malmö University
+   Course: DA281A Web Development II - Client - 
+   @date 10/09/2021
+   
+*/
+
+"use strict";
+
+//Uppgift8
+document.write('<h1>Uppgift 8!</h1>');
+
+var actors = ["Sherlock", "Watson", "Bo"];
+console.log(actors.join('-'));
