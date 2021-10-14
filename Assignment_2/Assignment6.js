881c3f7cec47b8af45af1ecbd00eab68d81e90c3
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-10-14 06:01:56 +0200

    final

---
+++ Assignment_2/Assignment6.js
@@ -0,0 +1,21 @@
+/*
+   @author: Elias Posluk
+   Student-id: AG3064
+   Uppgift: Assignment 2 part 6
+   Malmö University
+   Course: DA281A Web Development II - Client - 
+   @date 02/10/2021
+   
+*/
+
+"use strict";
+
+//Skapar ett objekt och sparar det i variabeln person
+var person = { 
+    firstName: "Elias", 
+    lastName: "Posluk", 
+    age: 32, 
+    family: ['Father', 'Mother', 'Sister', 'Brother']
+}; 
+//skriver ut innehållet på person.
+console.log(person);
